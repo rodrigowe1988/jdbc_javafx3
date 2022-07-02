33313634d8ba13f3842f5155b25aaa689861b1b8
rodrigowe1988 <rodrigoweber97@hotmail.com>
2022-07-02 17:50:29 -0300

    events component

---
+++ ANGULAR/curso-angular/curso-angular/src/app/components/events/events.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
+})
+export class EventsComponent implements OnInit {
+
+  show: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  showMessage(): void {
+    this.show = !this.show;
+  }
+
+}
