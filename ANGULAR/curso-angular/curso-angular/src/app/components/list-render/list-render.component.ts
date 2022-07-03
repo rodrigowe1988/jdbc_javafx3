74e5e36a675556dad2616a9039c390871729a12b
rodrigowe1988 <rodrigoweber97@hotmail.com>
2022-07-03 10:02:44 -0300

    interfaces

--- ANGULAR/curso-angular/curso-angular/src/app/components/list-render/list-render.component.ts
+++ ANGULAR/curso-angular/curso-angular/src/app/components/list-render/list-render.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -7,16 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListRenderComponent implements OnInit {
 
-  animals = [
-    { name: 'Turca', type: 'Dog'},
-    { name: 'Tom', type: 'Bird'},
-    { name: 'Frida', type: 'Cat'},
-    { name: 'Bob', type: 'Turtle'},
+  animals: Animal[] = [
+    { name: 'Turca', type: 'Dog', age: 10},
+    { name: 'Tom', type: 'Bird', age: 1},
+    { name: 'Frida', type: 'Cat', age: 6},
+    { name: 'Bob', type: 'Turtle', age: 5},
   ];
+
+  animal: Animal = {
+    name: 'Teste',
+    type: 'snake',
+    age: 2
+  }
+
+  animalDetails: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showAge(animal: Animal) {
+    this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
+    console.log(this.animalDetails);
   }
 
 }
