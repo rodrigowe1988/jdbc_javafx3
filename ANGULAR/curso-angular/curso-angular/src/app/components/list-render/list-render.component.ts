c1af5e22514ad870ce733e7c7e7b51cd33957365
rodrigowe1988 <rodrigoweber97@hotmail.com>
2022-07-03 14:21:30 -0300

    http requisitions angular

--- ANGULAR/curso-angular/curso-angular/src/app/components/list-render/list-render.component.ts
+++ ANGULAR/curso-angular/curso-angular/src/app/components/list-render/list-render.component.ts
@@ -11,22 +11,13 @@ import { ListService } from 'src/app/services/list.service';
 })
 export class ListRenderComponent implements OnInit {
 
-  animals: Animal[] = [
-    { name: 'Turca', type: 'Dog', age: 10},
-    { name: 'Tom', type: 'Bird', age: 1},
-    { name: 'Frida', type: 'Cat', age: 6},
-    { name: 'Bob', type: 'Turtle', age: 5},
-  ];
-
-  animal: Animal = {
-    name: 'Teste',
-    type: 'snake',
-    age: 2
-  }
+  animals: Animal[] = [];
 
   animalDetails: string = '';
 
-  constructor(private listService: ListService) { }
+  constructor(private listService: ListService) {
+    this.getAnimals();
+   }
 
   ngOnInit(): void {
   }
@@ -39,6 +30,10 @@ export class ListRenderComponent implements OnInit {
   removeAnimal(animal: Animal): void {
     console.log("Animal removed...")
     this.animals = this.listService.remove(this.animals, animal);
+  }
+
+  getAnimals(): void {
+    this.listService.getAll().subscribe((animals) => (this.animals = animals));
   }
 
 }
